beb0e3d0042fc67b339591776608f17e04c4a05e
hamatatu <67685642+hamatatu@users.noreply.github.com>
2020-10-02 17:33:08 +0900

    Add files via upload

--- js/compare.js
+++ js/compare.js
@@ -163,3 +163,42 @@ if (device == 'smt') {
 document.body.addEventListener(endEvent, dragEnd, false);
 window.addEventListener("load", loadFunc, false);
 window.addEventListener("resize", resizeFunc, false);
+
+
+/*
+*
+scroll-anim
+*
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  var markerText = document.querySelectorAll('.comparison-arrow'); // 監視対象を選択
+  var markerTextArr = Array.prototype.slice.call(markerText); // 監視対象をArrayに変換（IE対策）
+
+  /* IntersectionObserverに渡すコールバック関数 */
+  var cb = function(entries, observer) {
+    entries.forEach((entry) => {
+
+      if(entry.isIntersecting) {
+        /* 監視対象が画面内に入ったときのアクション */
+        entry.target.classList.add('inview'); // 画面内に入った要素にinviewクラスを付与
+        observer.unobserve(entry.target); // 1度発火した後監視を止める
+      }
+    });
+  }
+  
+  /* IntersectionObserverに渡すコールバック関数 */
+  var options = {
+    rootMargin: "-10px 0px"
+  }
+
+  /* IntersectionObserver初期化 */
+  var io = new IntersectionObserver(cb, options);
+  io.POLL_INTERVAL = 100; // Polyfillの設定
+
+  markerTextArr.forEach(el => {
+    io.observe(el);
+  });
+
+});
